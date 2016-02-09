73209a4a6319be6d2dfeb8904bddc9c96042113b
John Delgado <pexison@hotmail.com>
2016-02-09 12:01:57 -0430

    Basic views of logging and register created (w/o artwork). Next step: controller to show modal dialog in restore password view

---
+++ LogingViewController.swift
@@ -0,0 +1,41 @@
+//
+//  LogingViewController.swift
+//  Secuvecinos
+//
+//  Created by John on 8/2/16.
+//  Copyright © 2016 John. All rights reserved.
+//
+
+import UIKit
+
+class LogingViewController: UIViewController {
+    @IBOutlet weak var usuario: UITextField!
+
+    @IBAction func elquetuquieras(sender: AnyObject) {
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
