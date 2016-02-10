b8bd81d7731b69f4cbdada1606effb6cd6b6bbb7
John Delgado <pexison@gmail.com>
2016-02-10 13:36:33 -0430

    First attempt at a slide out menu

--- Secuvecinos/WelcomeViewController.swift
+++ Secuvecinos/WelcomeViewController.swift
@@ -1,29 +1,23 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  Secuvecinos
 //
-//  Created by John on 7/2/16.
+//  Created by John on 10/2/16.
 //  Copyright © 2016 John. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var Menu: UIBarButtonItem!
+class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        Menu.target = self.revealViewController()
-        Menu.action = Selector("revealToggle:")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
