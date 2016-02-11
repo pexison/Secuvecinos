c66006892c9a70f0e53f2bae7f1682925b358f0a
John Delgado <pexison@gmail.com>
2016-02-10 23:33:32 -0430

    Dos menus del slide out listos. Los otros tres crashean.

---
+++ Secuvecinos/BackTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  BackTableViewController.swift
+//  Secuvecinos
+//
+//  Created by John on 10/2/16.
+//  Copyright © 2016 John. All rights reserved.
+//
+
+import UIKit
+
+class BackTableViewController: UITableViewController {
+
+    var TableArray = [String]()
+    
+    override func viewDidLoad() {
+        TableArray = ["Usuario","Noticias","Reportar evento","Llamar a emergencias", "Configuración"]
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return TableArray.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier(TableArray[indexPath.row], forIndexPath: indexPath) as UITableViewCell
+        
+        cell.textLabel?.text = TableArray[indexPath.row]
+        
+        return cell
+    }
+    
+}
--- Secuvecinos/UserViewController.swift
+++ Secuvecinos/UserViewController.swift
@@ -1,17 +1,16 @@
 //
-//  ViewController.swift
+//  UsuarioViewController.swift
 //  Secuvecinos
 //
-//  Created by John on 7/2/16.
+//  Created by John on 10/2/16.
 //  Copyright © 2016 John. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class UserViewController: UIViewController {
 
     @IBOutlet weak var Menu: UIBarButtonItem!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,12 +19,5 @@ class ViewController: UIViewController {
         
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
-
