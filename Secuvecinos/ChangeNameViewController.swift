373ac2fe45a3cbc5899183c0d843b419d378b961
Gabriela Limonta <glimonta@gmail.com>
2016-02-13 01:23:43 -0430

    Almost all menu views for the modifying user data done. We're missing the select a picture one.

---
+++ Secuvecinos/ChangeNameViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ChangeNameViewController.swift
+//  Secuvecinos
+//
+//  Created by John on 13/2/16.
+//  Copyright © 2016 John. All rights reserved.
+//
+
+import UIKit
+
+class ChangeNameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+}
